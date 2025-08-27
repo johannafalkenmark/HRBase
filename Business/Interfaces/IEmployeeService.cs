f87c04356ee0f2e0a7a4aa2d156d38faa8cf3d08
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-08-27 11:47:12 +0200

    employeeservice and responseresult

---
+++ Business/Interfaces/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using Business.Models;
+using Business.Models.ResponseModels;
+
+namespace Business.Interfaces
+{
+    public interface IEmployeeService
+    {
+        Task<ResponseResult<Employee>> CreateEmployeeAsync(EmployeeRegistrationForm employeeRegistrationForm);
+        Task<ResponseResult<bool>> DeleteEmployeeAsync(int id);
+        Task<ResponseResult<IEnumerable<Employee>>> GetAllEmployeesAsync();
+        Task<ResponseResult<Employee>> GetEmployeeByIdAsync(int id);
+        Task<ResponseResult<bool>> UpdateEmployeeAsync(int id, EmployeeRegistrationForm employeeRegistrationForm);
+    }
+}
